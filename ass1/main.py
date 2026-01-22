c504c8cd24df54ab36e7ce6d35c156116e449bae
DanyGoT <danygonzteig@gmail.com>
2026-01-22 21:16:01 +0100

    feat(ass1): implement merge sort

--- ass1/main.py
+++ ass1/main.py
@@ -11,8 +11,35 @@ def insertion_sort(arr: List[int]) -> List[int]:
         arr[i + 1] = key
     return arr
 
-def merge_sort(arr: List[int]) -> List[int]:
-    raise NotImplementedError
+
+def merge_sort(arr: List[int], p=1, r=None) -> List[int]:
+    if r is None:
+        r = len(arr)
+
+    if p < r:
+        q = (p + r) // 2
+        arr = merge_sort(arr, p, q)
+        arr = merge_sort(arr, q + 1, r)
+        merge(arr, p, q, r)
+    return arr
+
+
+def merge(arr: List[int], p, q, r) -> List[int]:
+    n1 = q - p + 1
+    n2 = r - q
+    left = arr[p - 1 : q]
+    right = arr[q:r]
+
+    i = 0
+    j = 0
+    for k in range(p - 1, r):
+        if i < n1 and (j > n2 - 1 or left[i] <= right[j]):
+            arr[k] = left[i]
+            i = i + 1
+        else:
+            arr[k] = right[j]
+            j = j + 1
+    return arr
 
 
 def heap_sort(arr: List[int]):
