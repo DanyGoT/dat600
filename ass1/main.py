4e93014007b9cf7ce7dc678d907954a2920e8bc9
DanyGoT <danygonzteig@gmail.com>
2026-01-22 21:15:38 +0100

    feat(ass1): implement insertion sort

--- ass1/main.py
+++ ass1/main.py
@@ -2,8 +2,14 @@ from typing import List
 
 
 def insertion_sort(arr: List[int]) -> List[int]:
-    raise NotImplementedError
-
+    for j in range(1, len(arr)):
+        key = arr[j]
+        i = j - 1
+        while i >= 0 and arr[i] > key:
+            arr[i + 1] = arr[i]
+            i = i - 1
+        arr[i + 1] = key
+    return arr
 
 def merge_sort(arr: List[int]) -> List[int]:
     raise NotImplementedError
